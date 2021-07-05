90fa1fe5470a23fdeb43f4c28b10dea84521a710
prathmeshrajmane <prathmesh.rajmane1@gmail.com>
2021-07-05 14:48:23 +0530

    Update zone.sh

--- ShellScriptAssignment/19052021/zone.sh
+++ ShellScriptAssignment/19052021/zone.sh
@@ -2,11 +2,8 @@
 echo "Current TimeZone is"
 timedatectl
 echo ""
-echo "Do you want to change TimeZone to IST? [Y,n]"
+echo "Changing Timezone to IST"
 read input
-if [[ $input == "Y" || $input == "y" ]]; then
         timedatectl set-timezone "Asia/Kolkata"
 	echo "TimeZone sucesfully changed to IST"
-else
-        echo "don't change TimeZone"
-fi
+
